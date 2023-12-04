d1ab59b369be024722ba1f5bc183e3e66533926f
toandoan0301 <toan.doanchi044@gmail.com>
2023-12-04 14:54:02 +0700

    first commit

--- src/main/java/com/group4/controller/AuthenticationController.java
+++ src/main/java/com/group4/controller/AuthenticationController.java
@@ -1,2 +1,10 @@
-package com.group4.controller;public class AuthenticationController {
+package com.group4.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+public class AuthenticationController {
+
 }
