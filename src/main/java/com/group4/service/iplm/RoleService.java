85933ddedc7f76afbf4d42ea75f24a368d4b87c0
toandoan0301 <toan.doanchi044@gmail.com>
2023-12-04 15:42:27 +0700

    edit RoleRepository and RoleService, Add AuthenticationController

--- src/main/java/com/group4/service/iplm/RoleService.java
+++ src/main/java/com/group4/service/iplm/RoleService.java
@@ -1,24 +1,28 @@
 package com.group4.service.iplm;
 
 import com.group4.model.login.Role;
+import com.group4.repository.RoleRepository;
 import com.group4.service.IRoleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 @Service
 public class RoleService implements IRoleService {
+    @Autowired
+    private RoleRepository roleRepository;
     @Override
     public Iterable<Role> findAll() {
-        return null;
+        return roleRepository.findAll();
     }
 
     @Override
     public void save(Role role) {
-
+        roleRepository.save(role);
     }
 
     @Override
     public Role findByName(String name) {
-        return null;
+        return roleRepository.findByName(name);
     }
 }
