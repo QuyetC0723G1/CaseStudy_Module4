6f60a232a1d1801d2b53b85773d3dc41f7deffb0
toandoan0301 <toan.doanchi044@gmail.com>
2023-12-07 17:23:20 +0700

    thêm chức năng thay đổi thông tin user

--- src/main/java/com/group4/controller/AuthenticationController.java
+++ src/main/java/com/group4/controller/AuthenticationController.java
@@ -8,6 +8,7 @@ import com.group4.service.UserService;
 import com.group4.service.iplm.CustomerService;
 import com.group4.service.iplm.JwtService;
 import com.group4.service.iplm.RoleService;
+import net.sf.jsqlparser.statement.create.table.CreateTable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -56,38 +57,37 @@ public class AuthenticationController {
         Iterable<User> users = userService.findAll();
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
+
     @PostMapping("/register")
-    public ResponseEntity<Customer> createNewUser(@RequestBody Customer customer, BindingResult bindingResult) {
+    public ResponseEntity<?> createNewUser(@RequestBody Customer customer, BindingResult bindingResult) {
         if (bindingResult.hasFieldErrors()) {
             System.out.println("1");
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>("ero", HttpStatus.BAD_REQUEST);
         }
         User user = customer.getUser();
         Iterable<User> users = userService.findAll();
         for (User currentUser : users) {
             if (currentUser.getUsername().equals(user.getUsername())) {
-                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+                return new ResponseEntity<>("That username is already in use", HttpStatus.BAD_REQUEST);
             }
         }
         if (!userService.isCorrectConfirmPassword(user)) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>("Password confirmation is incorrect", HttpStatus.BAD_REQUEST);
         }
-        Role roleUser= roleService.findByName("ROLE_USER");
+        Role roleUser = roleService.findByName("ROLE_USER");
         Set<Role> roles = new HashSet<>();
-       if(user.getRoles().isEmpty()) {
-           roles.add(roleUser);
-           user.setRoles(roles);
-       }
-        if( customerService.emailIsUse(customer.getEmail())){
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        roles.add(roleUser);
+        user.setRoles(roles);
+        if (customerService.emailIsUse(customer.getEmail())) {
+            return new ResponseEntity<>("email has been used ", HttpStatus.BAD_REQUEST);
         }
-        if (customerService.phoneIsUse(customer.getPhoneNumber())){
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        if (customerService.phoneIsUse(customer.getPhoneNumber())) {
+            return new ResponseEntity<>("The phone number is already in use", HttpStatus.BAD_REQUEST);
         }
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setConfirmPassword(passwordEncoder.encode(user.getConfirmPassword()));
         User myUser = userService.save(user);
-        customer.setUser(myUser);
+        customer.setUser(user);
         customerService.save(customer);
         return new ResponseEntity<>(customer, HttpStatus.CREATED);
     }
@@ -100,7 +100,7 @@ public class AuthenticationController {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         User currentUser = userService.findByUsername(user.getUsername());
         Customer customer = customerService.findCustomerByUserId(currentUser.getId()).get();
-        return ResponseEntity.ok(new JwtResponse(jwt, currentUser.getId(), userDetails.getUsername(),customer.getFullName(), userDetails.getAuthorities()));
+        return ResponseEntity.ok(new JwtResponse(jwt, currentUser.getId(), userDetails.getUsername(), customer.getFullName(), userDetails.getAuthorities()));
     }
 
     @GetMapping("/users/{id}")
@@ -124,10 +124,43 @@ public class AuthenticationController {
         userService.save(user);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
-    @GetMapping("/customers/{id}")
-    public ResponseEntity<Customer> getCustomer(@PathVariable Long id){
-        Optional<Customer> customer = customerService.findOneById(id);
+
+    @GetMapping("/customers/{userId}")
+    public ResponseEntity<Customer> getCustomerByUserId(@PathVariable Long userId) {
+        Optional<Customer> customer = customerService.findCustomerByUserId(userId);
         return customer.map(value -> new ResponseEntity<>(value, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+    @PutMapping("/customers/{userId}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable Long userId, @RequestBody Customer customer) {
+        Optional<Customer> customerOptional = customerService.findCustomerByUserId(userId);
+        if (customerOptional.isPresent()) {
+            Customer customer1 = customerOptional.get();
+            customer1.setAddress(customer.getAddress());
+            customer1.setFullName(customer.getFullName());
+            customer1.setBirthDay(customer.getBirthDay());
+            customerService.save(customer1);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    @PutMapping("/change-password")
+    public ResponseEntity<?> changePassword(@RequestBody User user) {
+        if (!userService.isCorrectConfirmPassword(user)) {
+            return new ResponseEntity<>("Password confirmation is incorrect", HttpStatus.BAD_REQUEST);
+        }
+        Optional<User> userOptional = userService.findById(user.getId());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setConfirmPassword(passwordEncoder.encode(user.getConfirmPassword()));
+        if (userOptional.isPresent()) {
+            User user1 = userOptional.get();
+            if (!passwordEncoder.matches(user.getUsername(),user1.getPassword())) {
+                return new ResponseEntity<>("Old password is incorrect", HttpStatus.BAD_REQUEST);
+            }
+            user1.setPassword(user.getPassword());
+            user1.setConfirmPassword(user.getConfirmPassword());
+            userService.save(user1);
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        return new ResponseEntity<>("error user not found", HttpStatus.NOT_FOUND);
+    }
 }
